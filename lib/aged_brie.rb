3efe81428263c4316c39aad7184f17cacf18a35d
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 19:06:47 +0000

    brie object

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,2 +1,15 @@
 class AgedBrie
+  attr_reader :days_remaining, :quality
+
+  def initialize(opts)
+    @days_remaining = opts[:days_remaining]
+    @quality = opts[:quality]
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining < 0 && @quality < 50
+  end
 end
