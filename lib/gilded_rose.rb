443c402924009faa139f1ba3e6a0de8f57decd61
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 19:06:59 +0000

    backstage passes object

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -29,16 +29,6 @@ class GildedRose
     @quality += 1 if @days_remaining < 0 && @quality < 50
   end
 
-  def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
-    @quality = 0 if @days_remaining < 0
-  end
-
   def tick
     item.tick
   end
