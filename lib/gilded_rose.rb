2d928e75d7bd70d901ba917fd28c4c28c81169cb
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 17:41:25 +0000

    backstage tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,8 +26,11 @@ class GildedRose
 
   def backstage_tick
     @days_remaining -= 1
+    return if @quality >= 50
+
     @quality += 1
     @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining == 0
     @quality = 0 if @days_remaining < 0
   end
 
