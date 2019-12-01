dd8de25db02488091432ad60fb4ca60611b0365d
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 19:53:06 +0000

    init refactor

--- lib/sulfuras.rb
+++ lib/sulfuras.rb
@@ -1,9 +1,9 @@
 class Sulfuras
   attr_reader :days_remaining, :quality
 
-  def initialize(opts)
-    @days_remaining = opts[:days_remaining]
-    @quality = opts[:quality]
+  def initialize(days_remaining:, quality:)
+    @days_remaining = days_remaining
+    @quality = quality
   end
 
   def tick
