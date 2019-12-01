4978757206f6955027f4159d161121ddcf160a61
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 19:06:57 +0000

    backstage passes object

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -1,2 +1,18 @@
 class BackstagePasses
+  attr_reader :days_remaining, :quality
+
+  def initialize(opts)
+    @days_remaining = opts[:days_remaining]
+    @quality = opts[:quality]
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+    @quality = 0 if @days_remaining < 0
+  end
 end
