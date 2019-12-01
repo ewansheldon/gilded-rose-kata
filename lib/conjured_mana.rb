2c2e42ef7542d0f2d0e5c526b5fb511b12194959
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 19:50:30 +0000

    conjured tick

--- lib/conjured_mana.rb
+++ lib/conjured_mana.rb
@@ -10,7 +10,7 @@ class ConjuredMana
     @days_remaining -= 1
     return if @quality <= 0
 
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+    @quality -= 2
+    @quality -= 2 if @days_remaining <= 0
   end
 end
