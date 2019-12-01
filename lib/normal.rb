9d09dbd5f043d14e2254ba6f9c392ee6058021f4
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 18:00:50 +0000

    klass for

--- lib/normal.rb
+++ lib/normal.rb
@@ -1,16 +1,2 @@
 class Normal
-  attr_reader :name, :days_remaining, :quality
-
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
-  end
-
-  def tick
-    @days_remaining -= 1
-    return if @quality <= 0
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
-  end
 end
