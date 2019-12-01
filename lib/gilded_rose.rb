2c5163b649c8cc6292487f96e921dede5c907926
Ewan <ewan.s@cucumberwifi.io>
2019-12-01 17:29:13 +0000

    aged brie tick

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,6 +15,10 @@ class GildedRose
   end
 
   def aged_brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining < 0 && @quality < 50
   end
 
   def tick
