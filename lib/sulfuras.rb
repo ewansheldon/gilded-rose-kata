53c5798cc8af28ce74827bdaefd5a4e279148ebb
Ewan <ewan.s@cucumberwifi.io>
2019-12-02 10:51:59 +0000

    inherit item init

--- lib/sulfuras.rb
+++ lib/sulfuras.rb
@@ -1,11 +1,4 @@
 class Sulfuras
-  attr_reader :days_remaining, :quality
-
-  def initialize(days_remaining:, quality:)
-    @days_remaining = days_remaining
-    @quality = quality
-  end
-
   def tick
   end
 end
